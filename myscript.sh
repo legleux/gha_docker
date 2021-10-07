9f2875addb2bc6dc29902032905f308b888584fa
Michael Legleux <mlegleux@ripple.com>
2021-10-06 18:27:21 -0700

    safe artifacts

--- myscript.sh
+++ myscript.sh
@@ -1,2 +1,9 @@
 echo "I am from script."
 echo "$1"
+. /etc/os-release
+if [ "$ID" = "centos" ]; then
+  dist="Centos"
+elif [ "$ID" = "ubuntu" ]; then
+  dist="Ubuntu"
+fi
+echo "Hooray from $dist!"
